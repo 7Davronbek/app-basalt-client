5b69b2fe29dcba07e8cbeebf92346fd545885d72
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-02-26 19:50:47 +0500

    tmt

--- js/app.js
+++ js/app.js
@@ -1,24 +1,46 @@
 
 
-const mobileUl = document.querySelector('.mobileUl ul')
+// const mobileUl = document.querySelector('.mobileUl ul')
 
-const burgerHandlerOpen = () => {
-    mobileUl.classList.add('active')
+// const burgerHandlerOpen = () => {
+//     mobileUl.classList.add('active')
+// }
+
+// const burgerHandlerClose = () => {
+//     mobileUl.classList.remove('active')
+// }
+
+// window.onscroll = function() {
+//     scrollFunction()
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop < document.documentElement.scrollTop ) {
+//       document.querySelector('.Navbar').classList.add('actived')
+//   } else {
+//     document.querySelector('.Navbar').classList.remove('actived')
+//   }
+// }
+
+const spiner = document.querySelector(".spiner");
+
+document.addEventListener("load", () => {
+  spiner.style.display = "none ";
+});
+
+function navbar() {
+  const nav = document.querySelector(".navBar");
+  let scrolling = window.pageYOffset > 200;
+
+  nav.classList.toggle("active", scrolling);
 }
 
-const burgerHandlerClose = () => {
-    mobileUl.classList.remove('active')
-}
+window.addEventListener("scroll", () => {
+  navbar();
+});
 
-window.onscroll = function() {
-    scrollFunction()
-};
 
-function scrollFunction() {
-  if (document.body.scrollTop < document.documentElement.scrollTop ) {
-      document.querySelector('.Navbar').classList.add('actived')
-  } else {
-    document.querySelector('.Navbar').classList.remove('actived')
-  }
-}
-
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.burger').classList.toggle('burgered')
+    document.querySelector('.mobile').classList.toggle('active')
+})
